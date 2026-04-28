c178940ccfcb1c0f82c3a857600dac14a6ec328a
Berliana24 <berlianasimamora91@gmail.com>
2026-04-28 22:50:35 +0700

    test: add login test case

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,7 +1,24 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
+import { users } from '../test_data/users';
 
-test('open saucedemo', async ({ page }) => {
-  await page.goto('/');
+test.describe('Login Feature', () => {
 
+  users.forEach((data) => {
+    test(data.testScenario, async ({ page }) => {
+      const loginPage = new LoginPage(page);
+      await loginPage.goto();
+
+      await loginPage.login(data.username, data.password);
+
+      if (data.expected === 'PASS') {
+        await expect(page).toHaveURL('/inventory.html');
+      } else{~
+        await expect(
+          loginPage.getErrorMessage(data.message)
+        ).toBeVisible();
+      }
+    });
+  });
 });
